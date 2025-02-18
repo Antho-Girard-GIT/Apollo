b5451e9f4c15c0b8cb844bc0bf11b04c24ea38b6
Anthonny Girard <anthonnygirard@gmail.com>
2025-02-17 21:37:47 -0500

    Ajout de la map

--- Apollo/assets/js/functions.js
+++ Apollo/assets/js/functions.js
@@ -60,6 +60,12 @@ function slider() {
 	}
 }
 prevBtn.style.display = 'none';
+// Map
+var map = L.map('map').setView([51.505, -0.09], 13);
 
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
 
 });
